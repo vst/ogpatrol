c99d8a25d82af6d8fb8760ad086192a61c77ca98
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2024-08-24 22:57:08 +0800

    feat: read the DOM content of the active tab

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,44 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  browser.tabs.onActivated.addListener(({ tabId }) => {
+    process(tabId);
+  });
+
+  browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
+    if (changeInfo?.status === "complete") {
+      process(tabId);
+    }
+  });
 });
+
+async function process(tabId: number) {
+  // Get the tab:
+  const tab = await browser.tabs.get(tabId);
+
+  // Attempt to get the tab URL;
+  const url = tab.url ?? tab.pendingUrl;
+
+  // Return if we do not have a URL:
+  if (!url) {
+    return;
+  }
+
+  // Attempt to get the hostname:
+  const hostname = new URL(url).hostname;
+
+  // Return if we do not have a hostname:
+  if (!hostname) {
+    return;
+  }
+
+  // Cool, we have a nice tab!
+  console.log("Tab activated:", url, hostname);
+
+  const [{ result }] = await browser.scripting.executeScript({
+    target: { tabId },
+    func: () => {
+      return document.documentElement.outerHTML;
+    },
+  });
+
+  console.log(result);
+}
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
-export default defineConfig({});
+export default defineConfig({
+  manifest: {
+    permissions: ["scripting"],
+    host_permissions: ["<all_urls>"],
+  },
+});
